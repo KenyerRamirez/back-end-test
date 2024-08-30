4bdb9e9e8b9158769457eac325abcdaaa2b98aba
KenyerRamirez <kenyer_2002@hotmail.com>
2024-08-29 23:21:04 -0400

    Refactor (App and controllers): corrections

--- src/controllers/UsersController/UsersController.js
+++ src/controllers/UsersController/UsersController.js
@@ -2,8 +2,6 @@ import Usuario from "../../models/Users/Users.js";
 
 export const getEmployees = async (req, res) => {
   try {
-    // const permissions = await userWithPermission(id);
-    // if (permissions) {
     const usersData = await Usuario.find({ rol: "Employee" }).exec();
     res.status(200).json({
       status: 200,
@@ -11,12 +9,6 @@ export const getEmployees = async (req, res) => {
       data: usersData,
     });
     console.log("employees retrieved successfully");
-    // } else {
-    //   res.status(401).json({
-    //     status: 401,
-    //     message: "You don't have permissions to manage users",
-    //   });
-    // }
   } catch (error) {
     console.log("Was an error trying to get employees", error);
   }
